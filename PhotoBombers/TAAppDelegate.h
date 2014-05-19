3bc0be988fdc97b3cccd557c3789971a2a5c513c
Harish <harish.upad@gmail.com>
2014-05-19 18:12:41 -0400

    Initial Commit

---
+++ PhotoBombers/TAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TAAppDelegate.h
+//  PhotoBombers
+//
+//  Created by Harish Upadhyayula on 5/19/14.
+//  Copyright (c) 2014 tensor apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoBombers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoBombers
+//
+//  Created by Harish Upadhyayula on 5/19/14.
+//  Copyright (c) 2014 tensor apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TAAppDelegate class]));
+    }
+}
